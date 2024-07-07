d1f94057443687ba0b1e755b486370668eb64535
skimpaint <10572483+skimpaint@users.noreply.github.com>
2024-07-07 17:35:52 -0400

    Solved 01 through 03. Passed the test. Testing git add with folders.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(words, times) {
+    let repeatBuffer = ""; 
+    if (times < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < times; i++) {
+        repeatBuffer += words;
+    };
+    return repeatBuffer;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    reverseStringBuffer = '';
+    for (let i = 0; i < string.length; i++) {
+        reverseStringBuffer += string[string.length - 1 - i];
+    };
+    return reverseStringBuffer;
 };
 
 // Do not edit below this line
