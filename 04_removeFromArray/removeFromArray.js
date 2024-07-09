5fcf4a0fdfa3e0f0b44ea3e3f6ef7fd47beb08db
skimpaint <10572483+skimpaint@users.noreply.github.com>
2024-07-09 13:54:19 -0400

    Solved 04. Learned about using multiple (undefined) number of parameters in a function through ...name

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeThis) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let arrMatch = 0;
+        for (let j = 0; j < removeThis.length; j++) {
+            if (arr[i] === removeThis[j]) {
+                arrMatch++;
+                break;
+            }
+        }
+        if (arrMatch === 0) {
+            newArr.push(arr[i]);
+        }
+    };
+    return newArr;
 };
 
+//console.log(removeFromArray([1,2,3,4,3,8,48], 2, 3))
 // Do not edit below this line
 module.exports = removeFromArray;
