e08eff8c83680614a7835e064378e31d6d49d870
skimpaint <10572483+skimpaint@users.noreply.github.com>
2024-09-02 17:41:02 -0400

    finished javascript-exercises 05 (redo looking at solution) through 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(big, smoll) {
+    if (!Number.isInteger(big) || !Number.isInteger(smoll)) return "ERROR";
+    if (big < 0 || smoll < 0) return "ERROR";
+    if (big < smoll) {
+        const buffer = big;
+        big = smoll;
+        smoll = buffer;
+    }
 
+    let sum = 0;
+    for (let i = smoll;  i <= big; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempf) {
+  let rawtemp = (tempf - 32) * 5 / 9
+  let tempc = Math.round(rawtemp * 10)/10
+  return tempc
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempc) {
+  let rawtemp = (tempc * 9 / 5) + 32
+  let tempf = Math.round(rawtemp * 10)/10
+  return tempf
 };
 
 // Do not edit below this line
